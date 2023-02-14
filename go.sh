d188d8c99258d1bd29d6ddc3aab6a03eb712f8ab
hayao <hayao@fascode.net>
2023-02-14 23:42:07 +0900

    Update: Add go.sh help

--- go.sh
+++ go.sh
@@ -5,13 +5,29 @@ script_path="$(cd "$(dirname "${0}")" || exit 1; pwd)"
 cd "$script_path" || exit 1
 
 go_files=("${script_path}/main.go")
+mode="${1-""}"
 
-case "${1-""}" in
+{ [[ -n "$mode" ]] || shift 1; } || {
+    echo "Debug tool for lico"
+    echo
+    echo "Usage: $0 [mode] [lico-args]"
+    echo
+    echo "Mode:"
+    echo "  build    make executable file"
+    echo "  run      run lico"
+    echo "  drun     run lico with '-l $script_path/lico.list'"
+    exit 1
+}
+
+case "${mode}" in
     "build")
-        go build -o "${script_path}/lico" -- "${go_files[@]}"
+        go build -o "${script_path}/lico" -- "${go_files[@]}" "$@"
         ;;
     "run")
-        go run -- "${go_files[@]}"
+        go run -- "${go_files[@]}" "$@"
+        ;;
+    "drun")
+        "$script_path/$(basename "$0")" "run" -l "$script_path/lico.list" "$@"
         ;;
     "")
         exit 1
