dccfcf8dd5994bdc6a9333b659894c3ea9217059
hayao <hayao@fascode.net>
2023-02-16 12:09:03 +0900

    Add: prepare for ReplaceToTemplate

---
+++ conf/template.go
@@ -0,0 +1,25 @@
+package conf
+
+/*
+import (
+	df "github.com/Hayao0819/lico/dotfile"
+	"github.com/Hayao0819/lico/utils"
+//	"reflect"
+
+	
+)
+*/
+
+
+/*
+func ReplaceToTemplate(path string)(df.Path, error){
+	var parsed df.Path
+	dirInfo, err := utils.GetOSEnv()
+	if err == nil{
+		return parsed, err
+	}
+
+
+
+}
+*/
