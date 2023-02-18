64cb91ab1051dacaddae3e36b08133570a00947e
hayao <hayao@fascode.net>
2023-02-18 23:32:31 +0900

    Add: rmlink command

---
+++ cmd/rmlink.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+func rmLinkCmd() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "rmlink [ファイル]",
+		Short: "リンクを削除します",
+		Long: `ホームディレクトリからリンクを削除します。管理ディレクトリ内から実体を削除することはありません。`,
+		Args:    cobra.MinimumNArgs(1),
+		Aliases: []string{},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Fprintln(os.Stderr, "あとで実装する")
+			return nil
+		},
+	}
+
+	return &cmd
+}
+
+func init() {
+	root.AddCommand(rmLinkCmd())
+}
