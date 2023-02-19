6ac9cb905d2e473d77e12f8b5784974ef1b3a6df
hayao <hayao@fascode.net>
2023-02-19 11:33:21 +0900

    Update: Add command check

--- go.sh
+++ go.sh
@@ -14,19 +14,36 @@ mode="${1-""}"
     echo "Usage: $0 [mode] [lico-args]"
     echo
     echo "Mode:"
-    echo "  build    make executable file"
-    echo "  run      run lico"
-    echo "  drun     run lico with '-l $script_path/lico.list'"
-    echo "  fmt      run gofmt"
+    echo "  build        make executable file"
+    echo "  run          run lico"
+    echo "  drun         run lico with '-l $script_path/lico.list'"
+    echo "  fmt          run gofmt"
+    echo "  newcmd NAME  make new command from template"
     exit 1
 }
 
 shift 1
 
+call_myself(){
+    "${script_path}/$(basename "$0")" "$@"
+}
+
 build_cmd(){
+    check_cmd "goreleaser"
     goreleaser build --snapshot --clean --single-target >&2
     "${script_path}/getpath.py" 
 }
+
+check_cmd(){
+    [ -n "${1-""}" ] || return 1
+    if ! which "$1" >/dev/null 2>&1; then
+        echo "$1 command is not installed." >&2
+        return 1
+    fi
+    return 0
+}
+
+check_cmd go
 
 case "${mode}" in
     "build")
@@ -41,13 +58,16 @@ case "${mode}" in
         "$script_path/$(basename "$0")" "run" -l "$script_path/lico.list" "$@"
         ;;
     "fmt")
+        check_cmd "gofmt"
         go mod tidy
         gofmt -l -w .
         ;;
     "newcmd")
         go run -- "$script_path/tools/main.go" newcmd "$script_path/misc/cmd.go.template" "${script_path}/cmd/${1}.go" "$1"
         ;;
-    "")
+    *)
+        echo "No such command"
+        call_myself ""
         exit 1
         ;;
 esac
