e7aac09101aded6543b0e70b1c66dc6433e14a1a
hayao <hayao@fascode.net>
2023-02-22 22:24:19 +0900

    Clean: err variable scope

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"os"
-
 	"github.com/Hayao0819/lico/cmd"
 )
 
 func main() {
-	err := cmd.Execute()
-	if err != nil {
+	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
