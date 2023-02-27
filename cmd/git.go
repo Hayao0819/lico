5638171393e625612efdbf8e8237d61d5a9123eb
hayao <hayao@fascode.net>
2023-02-27 15:21:50 +0900

    Add: Add new git command

---
+++ cmd/git.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	//"fmt"
+
+	"github.com/Hayao0819/lico/utils"
+	"github.com/spf13/cobra"
+
+	//"github.com/Hayao0819/lico/conf"
+	"github.com/Hayao0819/lico/vars"
+)
+
+func gitCmd() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "git [git args]...",
+		Short: "gitを実行する",
+		Long: `設定ファイルリポジトリ内でGitコマンドを実行します`,
+		Args: cobra.ArbitraryArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if !hasCorrectRepoDir(){
+				return vars.ErrNoRepoDir
+			}
+
+			return utils.RunCmd("git" , append([]string{"-C", *repoDir}, args...)...)
+		},
+	}
+
+	return &cmd
+}
+
+func init() {
+	root.AddCommand(gitCmd())
+}
