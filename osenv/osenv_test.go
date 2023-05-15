3f97b3669843168178335a56257126dd12ec38fd
hayao <hayao@fascode.net>
2023-05-15 21:55:21 +0900

    Adds: osenv test

---
+++ osenv/osenv_test.go
@@ -0,0 +1,31 @@
+package osenv_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Hayao0819/lico/osenv"
+	"github.com/Hayao0819/lico/tester"
+)
+
+func TestMain(m *testing.M){
+	tester.CommonTestMain("../example")(m)
+}
+
+func TestGet(t *testing.T){
+	e, err := osenv.Get()
+	if err != nil{
+		t.Fatal(err)
+	}
+	for i, v := range e {
+		t.Logf("%s: %s", i, v)
+		if strings.TrimSpace(i) == ""{
+			t.Errorf("empty key: %s", i)
+			break
+		}
+		if strings.TrimSpace(v) == ""{
+			t.Errorf("empty value: %s = %s", i, v)
+			break
+		}
+	}
+}
