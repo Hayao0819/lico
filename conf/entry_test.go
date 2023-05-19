3e035105c3d272ae65c907be3b13346ac7c4676b
hayao <hayao@fascode.net>
2023-05-19 13:22:09 +0900

    Fix: Entry test

--- conf/entry_test.go
+++ conf/entry_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 
 	"github.com/Hayao0819/lico/conf"
+	p "github.com/Hayao0819/lico/paths"
 	"github.com/Hayao0819/lico/tester"
 	"github.com/Hayao0819/lico/vars"
-	p "github.com/Hayao0819/lico/paths"
 )
 
 func TestMain(m *testing.M){
@@ -21,36 +21,27 @@ type testEntry struct{
 	expect conf.Entry
 }
 
-var args = []testEntry{
-	{
-		repo: p.Path(path.Join(vars.RepoDir, "config", "example1.txt")),
-		home: p.Path(path.Join(vars.RepoDir, "your-home", "ex1.txt")),
-		index: 1,
-		expect: conf.Entry{
-			RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example1.txt")),
-			HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex1.txt")),
-		},
-	},
-	{
-		repo: p.Path(path.Join(vars.RepoDir, "config", "example2.txt")),
-		home: p.Path(path.Join(vars.RepoDir, "your-home", "ex2.txt")),
-		index: 2,
-		expect: conf.Entry{
-			RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example2.txt")),
-			HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex2.txt")),
-		},
-	},
-}
-
-func getEntry() []conf.Entry{
-	var entries []conf.Entry
-	for _, arg := range args {
-		entries = append(entries, arg.expect)
-	}
-	return entries
-}
-
 func TestNewEntry(t *testing.T){
+	var args = []testEntry{
+		{
+			repo: p.Path(path.Join(vars.RepoDir, "config", "example1.txt")),
+			home: p.Path(path.Join(vars.RepoDir, "your-home", "ex1.txt")),
+			expect: conf.Entry{
+				RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example1.txt")),
+				HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex1.txt")),
+			},
+		},
+		{
+			repo: p.Path(path.Join(vars.RepoDir, "config", "example2.txt")),
+			home: p.Path(path.Join(vars.RepoDir, "your-home", "ex2.txt")),
+			expect: conf.Entry{
+				RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example2.txt")),
+				HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex2.txt")),
+			},
+		},
+	}
+
+
 	for _, arg := range args {
 		entry := conf.NewEntry(arg.repo, arg.home)
 		if entry != arg.expect {
@@ -60,7 +51,28 @@ func TestNewEntry(t *testing.T){
 }
 
 func TestNewEntryWithIndex(t *testing.T){
-	//tester.MakeSymLinkInExample()
+	var args = []testEntry{
+		{
+			repo: p.Path(path.Join(vars.RepoDir, "config", "example1.txt")),
+			home: p.Path(path.Join(vars.RepoDir, "your-home", "ex1.txt")),
+			index: 1,
+			expect: conf.Entry{
+				RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example1.txt")),
+				HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex1.txt")),
+				Index: 1,
+			},
+		},
+		{
+			repo: p.Path(path.Join(vars.RepoDir, "config", "example2.txt")),
+			home: p.Path(path.Join(vars.RepoDir, "your-home", "ex2.txt")),
+			index: 2,
+			expect: conf.Entry{
+				RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example2.txt")),
+				HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex2.txt")),
+				Index: 2,
+			},
+		},
+	}
 	for _, arg := range args {
 		entry := conf.NewEntryWithIndex(arg.repo, arg.home, arg.index)
 		if entry != arg.expect {
@@ -75,13 +87,21 @@ func TestExistsRepoPath(t *testing.T){
 		expect bool
 	}
 
-	tests := []testcase{}
-
-	for _, e := range getEntry() {
-		tests = append(tests, testcase{
-			entry: e,
+	tests := []testcase{
+		{
+			entry:  conf.Entry{
+				RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example2.txt")),
+				HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex2.txt")),
+			},
 			expect: true,
-		})
+		},
+		{
+			entry: conf.Entry{
+				RepoPath: p.Path(path.Join(vars.RepoDir, "config", "example1.txt")),
+				HomePath: p.Path(path.Join(vars.RepoDir, "your-home", "ex1.txt")),
+			},
+			expect: true,
+		},
 	}
 
 	// add not exists path
