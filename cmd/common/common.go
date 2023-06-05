6c7f44d8946ce34653843a23c4b542b9165c9df2
hayao <hayao@fascode.net>
2023-06-05 22:46:10 +0900

    Fix: Normize ignore list

--- cmd/common/common.go
+++ cmd/common/common.go
@@ -8,21 +8,31 @@ import (
 func Normalize() error {
 	// 重要なパスを正規化
 	err := error(nil)
-
 	vars.RepoDir, err = utils.Abs("", vars.RepoDir)
 	if err != nil {
 		return err
 	}
 
-	
-	for _, v := range []*string{&vars.List, &vars.Ignore, &vars.Created, &vars.PkgList} {
+	// ignoreをコピー
+	ignore := vars.Ignore
+	vars.Ignore=[]string{}
+	for _, i := range ignore{
+		fixed, err := utils.Abs("", i)
+		if err == nil{
+			vars.Ignore = append(vars.Ignore, fixed)
+		}
+	}
+
+
+	// いろんなパスを正規化
+	for _, v := range []*string{&vars.List, &vars.Created, &vars.PkgList,} {
 		if utils.IsEmpty(*v) {
 			continue
 		}
 
 		*v, err = utils.Abs(vars.RepoDir, *v)
-		if err != nil {
-			*v = utils.ReplaceTilde(*v)
+		if err != nil{
+			return err
 		}
 	}
 	return nil
